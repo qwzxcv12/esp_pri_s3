3250ad1916ad3f1a29420ac7816697017feaeeee
ESP Builder <builder@esp.local>
2026-07-18 11:21:21 +0700

    Build trigger

--- dns_server.h
+++ dns_server.h
@@ -1,9 +1,17 @@
 #pragma once
 #include "esp_err.h"
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 // Khởi động DNS server captive portal (chạy trong 1 task riêng)
 // Mọi query DNS sẽ được trả lời bằng resolved_ip
 esp_err_t dns_server_start(uint32_t resolved_ip);
 
 // Dừng DNS server
 void dns_server_stop(void);
+
+#ifdef __cplusplus
+}
+#endif
