0565406c61990c442e53368c8338fbc59193e7ea
ESP Builder <builder@esp.local>
2026-07-18 11:04:30 +0700

    Build trigger

--- pri_NOW.cpp
+++ pri_NOW.cpp
@@ -1319,11 +1319,13 @@ extern "C" void app_main(void)
 
         wifi_config_t wifi_config = {};
         const char* ap_ssid = "ESP32_WiFi_Config";
+        const char* ap_pass = "12345678";
         strncpy((char*)wifi_config.ap.ssid, ap_ssid, sizeof(wifi_config.ap.ssid));
+        strncpy((char*)wifi_config.ap.password, ap_pass, sizeof(wifi_config.ap.password));
         wifi_config.ap.ssid_len = strlen(ap_ssid);
         wifi_config.ap.channel = 6;
         wifi_config.ap.max_connection = 4;
-        wifi_config.ap.authmode = WIFI_AUTH_OPEN;
+        wifi_config.ap.authmode = WIFI_AUTH_WPA2_PSK;
 
         // Explicitly set static IP for AP and restart DHCP
         esp_netif_dhcps_stop(ap_netif);
